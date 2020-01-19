43adb6b37011fd2d15d07c85938f7b1dfdf72da8
Shaqqour <mshaqqourm@gmail.com>
2020-01-19 13:34:48 -0800

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,13 +2,15 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+  
+  accepts_nested_attributes_for :notes, :reject_if => proc { |attrs| attrs[:content].blank? }
 
-  def artist_name=(artist_name)
-    self.artist = Artist.find_or_create_by(name: artist_name)
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
 
   def artist_name
-    self.artist.name
+    self.artist.try(:name)
   end
 
 end
